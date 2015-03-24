0348c73bab7460e6b3a2b899904e0b189a9e9be6
neilw22 <neilw22@gmail.com>
2015-03-24 14:15:19 -0400

    Hadouken sound and animation

--- js/app.js
+++ js/app.js
@@ -8,10 +8,16 @@ $(document).ready(function() {
 	$('.ryu-still').show();
   })
   .mousedown(function() {
-    // play hadouken sound
+    playHadouken();
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
-    $('.hadouken').show();
+    $('.hadouken').finish().show().animate(
+    	{'left': '1020px'}, 500,
+    	function() {
+    		$(this).hide();
+    		$(this).css('left', '600px');
+    	}
+    	);
     // show hadouken and animate it to the right of the screen
   })
   .mouseup(function() {
@@ -20,3 +26,10 @@ $(document).ready(function() {
     // ryu goes back to his ready position
   });
 });
+
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
