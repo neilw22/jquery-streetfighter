de55e634fb46e0d82de610a5021386de4b7169c4
neilw22 <neilw22@gmail.com>
2015-03-30 14:02:01 -0400

    Mouseover debbuged

--- js/app.js
+++ js/app.js
@@ -1,58 +1,58 @@
 $(document).ready(function() {
-  $('.ryu').mouseenter(function() {
+
+	var isMouseOver = false;
+	
+    $('.ryu').mouseenter(function() {
     $('.ryu-still').hide();
     $('.ryu-ready').show();
+    isMouseOver = true;
   })
   .mouseleave(function() {
     $('.ryu-ready').hide();
 	$('.ryu-still').show();
+	isMouseOver = false;
   })
   .mousedown(function() {
     playHadouken();
     $('.ryu-ready').hide();
+    $('.ryu-cool').hide();
     $('.ryu-throwing').show();
-    $('.hadouken').finish().show().animate(
-    	{'left': '1020px'}, 500,
-    	function() {
+    $('.hadouken').finish().show().animate({
+    	'left': '1020px'
+    }, 500, function() {
     		$(this).hide();
     		$(this).css('left', '600px');
-    	}
-    	);
+    });
   })
   .mouseup(function() {
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
   });
 
-  $('body').on('keydown keyup',function(e){
-      var show = e.type=="keydown" ? 'show' : 'hide' ;
-      if(e.which==88){
-          $(this).css(
-          	$('.ryu-ready').hide();
-  			$('.ryu-still').hide();
-			$('.ryu-cool').show(););  
-      }
-    });
-
-  /*$('body').keydown(function() {
-  	//console.log('keydown');
+  $('body').keydown(function(event) {
+  	if(event.which==88) {
   	$('.ryu-ready').hide();
   	$('.ryu-still').hide();
+  	$('.ryu-throwing').hide();
 	$('.ryu-cool').show();
-  	//hide other ryus on keydown
-  	//show ryu cool on keydown
-  	//show ryu cool on keydown id=88
+	}
   })
   	$('body').keyup(function() {
   	//console.log('keydown');
-  	$('.ryu-ready').hide();
-  	$('.ryu-still').show();
-	$('.ryu-cool').hide();
+  	if (isMouseOver==true) {
+  		$('.ryu-still').hide();
+  		$('.ryu-ready').show();
+  		$('.ryu-throwing').hide();
+  		$('.ryu-cool').hide();
+  		}
+  	else {
+  		$('.ryu-throwing').hide();
+  		$('.ryu-still').show();
+		$('.ryu-cool').hide();
+		$('.ryu-ready').hide();
+		}
   });
-*/
-
 });
-
 
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
